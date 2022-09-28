607e922799ccd00899a62ede7fa86d379f954141
Wehde <kilianwehde@outlook.com>
2022-09-27 23:09:49 -0500

    Initial Commit with Admin and Routing

---
+++ MTG-Card-Collection-App/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MTG_Card_Collection_App.Models;
+using System.Diagnostics;
+
+namespace MTG_Card_Collection_App.Admin.Controllers
+{
+    [Area("Admin")]
+
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [Route("[area]/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("[area]/Privacy")]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [Route("[area]/Collection")]
+        public IActionResult Collection()
+        {
+            return View();
+        }
+
+        [Route("[area]/Decks")]
+        public IActionResult Decks()
+        {
+            return View();
+        }
+
+        [Route("[area]/Social")]
+        public IActionResult Social()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult StaticContent(string num)
+        {
+            return Content($"Static Content: {num}");
+        }
+    }
+}
---
+++ MTG-Card-Collection-App/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MTG_Card_Collection_App.Models;
+using System.Diagnostics;
+
+namespace MTG_Card_Collection_App.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("Privacy")]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [Route("Collection")]
+        public IActionResult Collection()
+        {
+            return View();
+        }
+
+        [Route("Decks")]
+        public IActionResult Decks()
+        {
+            return View();
+        }
+
+        [Route("Social")]
+        public IActionResult Social()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+    }
+}
