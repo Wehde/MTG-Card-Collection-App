c177e54fe8b888ca2b687a42eab7dc981f70e4d6
Wehde <kilianwehde@outlook.com>
2022-11-27 21:25:05 -0600

    Added button tag helper for buttons without a class.

---
+++ MTG-Card-Collection-App/TagHelpers/ButtonTagHelper.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace MTG_Card_Collection_App.TagHelpers
+{
+    public class ButtonTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (context.AllAttributes.ContainsName("class"))
+            {
+
+            } else
+            {
+                output.Attributes.SetAttribute("class", "btn btn-primary");
+            }
+            
+        }
+    }
+}
