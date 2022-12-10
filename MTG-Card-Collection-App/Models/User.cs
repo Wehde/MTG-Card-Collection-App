66099e8c25961f8b663f1ed5930cb0d2ac12f083
Wehde <kilianwehde@outlook.com>
2022-12-09 20:57:19 -0600

    Merge Troubles

--- MTG-Card-Collection-App/Models/User.cs
+++ MTG-Card-Collection-App/Models/User.cs
@@ -1,9 +1,15 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MTG_Card_Collection_App.Models
 {
     public class User : IdentityUser
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        [NotMapped]
+        public IList<string> RoleNames { get; set; }
 
     }
 }
