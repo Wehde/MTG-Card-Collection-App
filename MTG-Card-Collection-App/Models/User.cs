12e242b6918ac44591860842a0a4b6fef836dac7
Wehde <kilianwehde@outlook.com>
2022-12-09 21:28:53 -0600

    Removed First and Last name

--- MTG-Card-Collection-App/Models/User.cs
+++ MTG-Card-Collection-App/Models/User.cs
@@ -4,12 +4,8 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace MTG_Card_Collection_App.Models
 {
     public class User : IdentityUser
-    {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-
+    {   
         [NotMapped]
         public IList<string> RoleNames { get; set; }
-
     }
 }
